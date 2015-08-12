f7067fb6e1357bdf5539d905586612f89540b42e
Bravo <Bravo@Yanilizs-MacBook-Pro.local>
2015-08-12 18:10:27 -0400

    js underline and hide

--- js/main.js
+++ js/main.js
@@ -1,12 +1,44 @@
 //this is where you define your functions
 
 // this is the syntax for defining a function:
-function functionName(parameters) {
-  //logic goes inside here
+ function addUnderline ($element) {
+  $element.addClass('underline');
 }
 
+function removeSiblingUnderline($element) {
+  var $current = $element;
+  var $parent = $element.parent();
+  var $siblings = $parent.siblings();
+  $siblings.children().removeClass('underline');
+
+}
+
+function toggleButton($element){
+  $('#showHide').toggle();
+  if($element.text() === "MEET THE TEAM" ) {
+    $element.text("THIS IS OUR TEAM");
+  } else {
+    $element.text("MEET THE TEAM");
+  }
+}
+
+//-------------Main-----------------------------------
 
 $(document).ready(function() {
   // this is where you put your event listeners
+  $('#showHide').hide();
+
+
+  $('.nav-link').click(function() {
+   removeSiblingUnderline($(this));
+   addUnderline($(this));
+
+  });
+
+  $('.button').click(function() {
+  toggleButton($(this));
+
+});
+
 
 });
